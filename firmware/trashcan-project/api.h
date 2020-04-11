ec8cd0685b6af5d5236de48c50f01f02ab276818
ezhov93 <ezhov93@users.noreply.github.com>
2020-04-11 14:59:42 +0300

    Update api.h

--- firmware/trashcan-project/api.h
+++ firmware/trashcan-project/api.h
@@ -1,5 +1,10 @@
+#ifndef API_H
+#define API_H
+
 typedef bool boolean;
 
 #define DISABLE_COPY(Class)      \
   Class(const Class &) = delete; \
   Class operator=(const Class &) = delete
+
+#endif  // API_H
